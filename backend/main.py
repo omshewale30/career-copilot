598820fd0bc5d46fd623bdb74906384780203238
omshewale <omshewale030@gmail.com>
2025-05-22 19:55:40 -0400

    changed CORS config

--- backend/main.py
+++ backend/main.py
@@ -12,40 +12,39 @@ async def lifespan(app: FastAPI):
     yield
     print("Stopping lifespan")
 
-
-
 app = FastAPI(
     title="Career Copilot API",
     description="API for Career Copilot",
     version="0.1.0",
-    lifespan= lifespan
+    lifespan=lifespan
 )
+
+# Define allowed origins
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
     "http://localhost:5173",
     "https://career-copilot-nu.vercel.app",
-    "https://career-copilot-frontend-ze2k7.kinsta.app"
+    "https://career-copilot-frontend-ze2k7.kinsta.app",
+    "https://career-copilot-backend-ze2k7.kinsta.app"
 ]
 
+# Add CORS middleware with specific configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
 )
 
 app.include_router(api_router)
 
 #app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-
 @app.get("/")
 async def root():
     return {"message": "Career Copilot API"}
-
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
